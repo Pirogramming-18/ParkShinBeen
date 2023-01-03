0e4774de53bbf0fc887c78062674daca426cc58c
spark2357 <psbted@naver.com>
2023-01-04 07:52:54 +0900

    JavaScript - display items

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -6,13 +6,26 @@ function loadItems(){
     .then(json => json.items);
 }
 
+// Update the list with the given items
+function displayItems(items){
+    const contanier = document.querySelector('.items');
+    contanier.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
 
+// Create HTML list item from the give data item
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
 
 // main
 loadItems()
 .then(items => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items)
 })
 .catch(console.log())

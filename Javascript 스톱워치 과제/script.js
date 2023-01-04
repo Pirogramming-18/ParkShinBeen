98a4aea8868c1791e4cf0c1444ef04598d206830
spark2357 <psbted@naver.com>
2023-01-05 01:39:27 +0900

    완성! 아래쪽 선택 개수에 따라 전체 선택 자동 활성화/비활성화도 만들었다.

--- Javascript 스톱워치 과제/script.js
+++ Javascript 스톱워치 과제/script.js
@@ -1,12 +1,17 @@
 let seconds = 0;
 let millis = 0;
 let intervalId;
+let selectOneBtn;
+let selectCount = 0;
+let length = 0;
 
 const displaySec = document.querySelector('.clock .sec');
 const displayMsec = document.querySelector('.clock .msec');
 const startBtn = document.querySelector('.buttons .start');
 const stopBtn = document.querySelector('.buttons .stop');
 const resetBtn = document.querySelector('.buttons .reset');
+const selectAllBtn = document.querySelector('.navbar .selectAllBtn i');
+const clearBtn = document.querySelector('.clear');
 const lapList = document.querySelector('.laps');
 
 function moveClock(){
@@ -27,6 +32,11 @@ startBtn.onclick = () => {
 stopBtn.onclick = () => {
     clearInterval(intervalId);
     displayLap();
+    selectOneBtn = document.querySelector('.laps');
+    selectOneBtn.removeEventListener('click', selectBtnClick);
+    selectOneBtn.addEventListener('click', selectBtnClick);
+    length++;
+    checkFull();
 };
 
 resetBtn.onclick = () => {
@@ -40,8 +50,8 @@ resetBtn.onclick = () => {
 function displayLap(){
     lapList.innerHTML = lapList.innerHTML + `
     <li class="lap">
-        <button type="button" class="select selectBtn iconBtn">
-            <i class="fa-regular fa-circle"></i>
+        <button type="button" class="selectOne iconBtn">
+            <i class="fa-regular fa-circle" id="icon"></i>
         </button>
         <span class="sec">${displaySec.textContent}</span>
         <span>:</span>
@@ -51,8 +61,83 @@ function displayLap(){
     lapList.scrollTop = lapList.scrollHeight;
 }
 
-// 전체 선택하면 아이콘 다 바뀜, 전체 선택
+function selectBtnClick(e){
+    if(e.target.closest('i')){
+        const selectedLap = e.target.closest('li');
+        const selectedIcon = e.target;
+        if(selectedLap.classList.contains('.checked')){
+            selectedLap.classList.remove('.checked');
+            changeCheckIcon(selectedIcon, 0);
+            selectCount--;
+        }
+        else{
+            selectedLap.classList.add('.checked');
+            changeCheckIcon(selectedIcon, 1);
+            selectCount++;
+        }
+        checkFull();
+    }
+}
 
-// 선택하면 아이콘 바뀜
+function changeCheckIcon(item, check){
+    if (check == 1){
+        item.classList.remove('fa-circle');
+        item.classList.add('fa-circle-check');
+    }
+    else{
+        item.classList.remove('fa-circle-check');
+        item.classList.add('fa-circle');
+    }
+}
 
-// clear Button 누르면 선택된 항목 삭제
+selectAllBtn.onclick = () => {
+    let check = 0;
+    const allBtnLi = selectAllBtn.closest('button');
+    const btnList = document.querySelectorAll('.selectOne i');
+    btnList.forEach(target => {
+        const oneBtnLi = target.closest('li');
+        if(allBtnLi.classList.contains('.checked')){
+            check = 0;
+            if(oneBtnLi.classList.contains('.checked')){
+                oneBtnLi.classList.toggle('.checked');
+                changeCheckIcon(target, 0);
+                selectCount--;
+            }
+        }
+        else{
+            check = 1;
+            if(!oneBtnLi.classList.contains('.checked')){
+                oneBtnLi.classList.toggle('.checked');
+                changeCheckIcon(target, 1);
+                selectCount++;
+            }
+        }
+    });
+    allBtnLi.classList.toggle('.checked');
+    changeCheckIcon(selectAllBtn, check);
+}
+
+clearBtn.onclick = () => {
+    const allBtnLi = selectAllBtn.closest('button')
+    allBtnLi.classList.remove('.checked');
+    changeCheckIcon(selectAllBtn, 0);
+    let lapLi = document.querySelectorAll('.laps li');
+    lapLi.forEach(target => {
+        if(target.classList.contains('.checked')){
+            target.remove();
+            selectCount--;
+            length--;
+        }
+    })
+}
+
+function checkFull(){
+    if(selectCount == length){
+        selectAllBtn.closest('button').classList.add('.checked');
+        changeCheckIcon(selectAllBtn, 1);
+    }
+    else{
+        selectAllBtn.closest('button').classList.remove('.checked');
+        changeCheckIcon(selectAllBtn, 0);
+    }
+}

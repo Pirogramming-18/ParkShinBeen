82f0963595e78798f3e88f69235cc7b7fc8452bd
spark2357 <psbted@naver.com>
2023-01-06 09:02:42 +0900

    1-8

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -10,31 +10,35 @@ def printWinner(player):
     print("win!")
 
 
-num = 0
-go = 0
-player = False
-while num < 31:
-    while True:
-        go = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+def brGame():
+    num = 0
+    go = 0
+    player = False
+    while num < 31:
+        while True:
+            go = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-        try:
-            int(go)
-        except ValueError:
-            print("정수를 입력하세요")
-        else:
-            go = int(go)
-            if 1 <= go <= 3:
-                break
+            try:
+                int(go)
+            except ValueError:
+                print("정수를 입력하세요")
             else:
-                print("1,2,3 중 하나를 입력하세요")
+                go = int(go)
+                if 1 <= go <= 3:
+                    break
+                else:
+                    print("1,2,3 중 하나를 입력하세요")
 
-    player = not player
+        player = not player
 
-    for i in range(go):
-        num += 1
-        printPlayer(player)
-        print(":", num)
-        if num == 31:
-            break
+        for i in range(go):
+            num += 1
+            printPlayer(player)
+            print(":", num)
+            if num == 31:
+                break
 
-printWinner(not player)
+    printWinner(not player)
+
+
+brGame()

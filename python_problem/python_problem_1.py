8a9b43591f50c488ef725af0909507ddf5bef5fa
spark2357 <psbted@naver.com>
2023-01-06 09:01:49 +0900

    1-6

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,8 +1,8 @@
 num = 0
 go = 0
-player = True
+player = False
 
-while True:
+while num < 31:
     while True:
         go = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
@@ -17,10 +17,13 @@ while True:
             else:
                 print("1,2,3 중 하나를 입력하세요")
 
+    player = not player
+
     for i in range(go):
         num += 1
         if player:
             print("playerA :", num)
         else:
             print("playerB :", num)
-    player = not player
+        if num == 31:
+            break
